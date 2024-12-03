162b5c06c9af333f5ab73471d976a5df19ba19ae
nick <nikdanelia@gmail.com>
2024-12-03 18:25:53 +0400

    day 3, part 2

--- day3/part2.js
+++ day3/part2.js
@@ -2,18 +2,20 @@ const fs = require('fs')
 let pattern = /mul\(\d*,\d*\)|do\(\)|don't\(\)/g
 let values = /(\d+)\s*,\s*(\d+)/
 fs.readFile('./input.txt', 'utf8', (err, data) => {
-  spl = data.split('\n')
+  if (err) throw err
+  const spl = data.split('\n')
   let sum = 0
+  let shouldSum = true
   spl.forEach((el) => {
-    let shouldSum = true
+    let matches
     while ((matches = pattern.exec(el)) != null) {
-      if (matches[0] == 'do()') {
+      if (matches[0] === 'do()') {
         shouldSum = true
-      } else if (matches[0] == "don't()") {
+      } else if (matches[0] === "don't()") {
         shouldSum = false
-      } else {
-        if (shouldSum) {
-          nums = values.exec(matches[0])
+      } else if (shouldSum) {
+        const nums = values.exec(matches[0])
+        if (nums) {
           sum += nums[1] * nums[2]
         }
       }
